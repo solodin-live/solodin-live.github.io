ed91eb4d237b043bd23c844d9957ef93f1fc3f42
Almaz Murzabekov <almaz@murzabekov.net>
2025-07-20 11:11:28 +0200

    fix: sitemap.xml entry deduplication

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,23 +1,33 @@
 import { videosData } from "@/data/videos-data";
 
 export default function sitemap() {
-  const videos = videosData.map((video) => {
-    return {
-      url: `https://solodin.live/video/${video.id}`,
-      lastModified: new Date(),
-    };
-  });
-
-  const tickers = videosData.flatMap((video) => {
-    return video.sections.flatMap((section) => {
-      return section.tickers.map((ticker) => {
-        return {
-          url: `https://solodin.live/ticker/${ticker}`,
+  const videos = Array.from(
+    new Map(
+      videosData.map((video) => [
+        `https://solodin.live/video/${video.id}`,
+        {
+          url: `https://solodin.live/video/${video.id}`,
           lastModified: new Date(),
-        };
-      });
-    });
-  });
+        },
+      ])
+    ).values()
+  );
+
+  const tickers = Array.from(
+    new Map(
+      videosData.flatMap((video) =>
+        video.sections.flatMap((section) =>
+          section.tickers.map((ticker) => [
+            `https://solodin.live/ticker/${ticker}`,
+            {
+              url: `https://solodin.live/ticker/${ticker}`,
+              lastModified: new Date(),
+            },
+          ])
+        )
+      )
+    ).values()
+  );
 
   return [
     {
